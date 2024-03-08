4bf342a728bdcbdf7b0a71c0dc9e1d48aabff928
BiagioCaputo <scebiagio98@gmail.com>
2024-03-08 17:05:37 +0100

    dinamic data added in the bottom part of the comics

--- resources/views/includes/comics/show.blade.php
+++ resources/views/includes/comics/show.blade.php
@@ -13,9 +13,9 @@
         </figure>
     </div>
 </div>
-<section id="comic-details" class="container">
-    <div class="upper-comic-details">
-        <div class="comic-generics">
+<section id="comic-details" >
+    <div class="upper-comic-details container" >
+        <div class="upper-comic-generics">
             <h1>{{ $comics['title'] }}</h1>
             <div class="availability">
                 <div class="availability-left">
@@ -31,6 +31,47 @@
         <figure class="advertisement">
             <img src=" {{ asset('images/adv.jpg')}}" alt="adv">
         </figure>
+    </div>
+    <div class="bottom-comic-details">
+        <div class="container">
+            <div class="bottom-comic-generics">
+                <div class="talents">
+                    <h2>Talents</h2>
+                    <div class="talents-box">
+                        <h5>Art by:</h5>
+                        <div class="artists-list">
+                            @foreach ($comics['artists'] as $artist)
+                            <span><a>{{$artist}}</a>@if(!$loop->last),@endif</span>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="talents-box">
+                        <h5>Written by:</h5>
+                        <div class="writers-list">
+                            @foreach ($comics['writers'] as $artist)
+                            <span><a>{{$artist}}</a>@if(!$loop->last)</a>,@endif</span>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="specs">
+                    <h2>Specs</h2>
+                    <div class="specs-box">
+                        <h5>Series:</h5>
+                        <h4>{{$comics['series']}}</h4>
+                    </div>
+                    <div class="specs-box">
+                        <h5>U.S. Price:</h5>
+                        <h4>{{$comics['price']}}</h4>
+                    </div>
+                    <div class="specs-box">
+                        <h5>On Sale Date:</h5>
+                        <h4>{{$comics['sale_date']}}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </section>
 
